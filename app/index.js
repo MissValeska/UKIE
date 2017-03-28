c5583483cbd5c54e3b8e0c3b7212040ebec84313
Valeska Victoria <missvaleskavv@gmail.com>
2017-03-27 22:18:27 -0700

    Adding an interface with which to interact with ukie.js, thus providing the server to listen for incoming requests, such as to the checkConjugations function.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
+var express = require('express')
+var bodyParser = require('body-parser');
 const ukie = require('./ukie')
+
+var app = express()
+
+app.get('/gettext', function (req, res) {
+  res.send(ukie.getText())
+})
+
+app.get('/checkconj', function (req, res) {
+  res.send(ukie.checkConjugations(req.body.text))
+})
+
+app.get('/checksentence', function (req, res) {
+  res.send(ukie.checkSentences(req.body.text))
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})

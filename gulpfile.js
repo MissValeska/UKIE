c4692cbc26856f355a206d679f9291ce2b179639
Gabriella Fabwen <geccentrical@gmail.com>
2017-08-19 03:00:12 -0700

    changed a lot of things in the profile, changed the glossary template, and improved css structure by adding gulp that handles conversion of sass/scss to css

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('public/static/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/static/css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('public/static/sass/**/*.scss',['styles']);
+});

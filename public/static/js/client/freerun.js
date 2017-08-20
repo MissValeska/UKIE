445665e31db108de2621087e169f8d1e5324a11d
Valeska <missvaleskavv@gmail.com>
2017-08-20 00:57:35 -0700

    Added freerun

--- public/static/js/client/freerun.js
+++ public/static/js/client/freerun.js
@@ -10,31 +10,41 @@ var config = {
 };
 firebase.initializeApp(config);
 
+function stuff(snapshot, num) {
 
-function freerun() {
+  var rnd = Math.floor(Math.random() * num) + 1;
 
-  console.log("Yo!");
+  console.log("Stuff!" + rnd);
 
-  var sentenceUA = "Rawr guys! ^-^";
+  var sentenceEN = snapshot.child("question" + rnd).val().sentenceEN;
   var wasClicked = false;
+  var wasClicked1 = false;
 
-  var correctText = "Something";
+  var correctText = snapshot.child("question" + rnd).val().word;
 
-  var wordType = "Noun";
-  var definition = "A thing";
+  var splitted = snapshot.child("question" + rnd).val().sentenceUA.split("+");
 
-  document.getElementById("inputLabel1").innerHTML = "Hey guys!";
-  document.getElementById("inputLabel2").innerHTML = "Wowza!";
+  var label1 = splitted[0];
+  var label2 = "";
+  if(splitted.length == 2) {
+    label2 = splitted[1];
+  }
+
+  var wordType = snapshot.child("question" + rnd).val().type;
+  var definition = snapshot.child("question" + rnd).val().definition;
+
+  document.getElementById("inputLabel1").innerHTML = label1;
+  document.getElementById("inputLabel2").innerHTML = label2;
 
   $('#enBtn').click(function(e){
     e.preventDefault();
     if(wasClicked) {
       wasClicked = false;
-      document.getElementById("sentenceUA").innerHTML = "";
+      document.getElementById("sentenceEN").innerHTML = "";
     }
     else {
       wasClicked = true;
-      document.getElementById("sentenceUA").innerHTML = sentenceUA;
+      document.getElementById("sentenceEN").innerHTML = sentenceEN;
     }
   });
 
@@ -42,15 +52,47 @@ function freerun() {
   document.getElementById("definition").innerHTML = "Definition: " + definition;
 
   $('#peekBtn').click(function(e){
-    document.getElementById("inputText").setAttribute("value", correctText);
+    if(wasClicked1) {
+      wasClicked1 = false;
+      document.getElementById("inputText").placeholder = "";
+    }
+    else {
+      wasClicked1 = true;
+      document.getElementById("inputText").value = "";
+      document.getElementById("inputText").setAttribute("placeholder", correctText);
+    }
   });
 
   $('#nextBtn').click(function(e){
     var omg = $('#inputText').val();
-    if(omg == correctText) {
-      console.log("You're correct!");
-    }
     console.log("omg:" + omg);
+    if(document.getElementById('nextBtn').innerHTML == "Next") {
+      document.getElementById("inputText").value = "";
+      document.getElementById("inputText").placeholder = "";
+      document.getElementById('nextBtn').innerHTML = "Check"
+      stuff(snapshot, num);
+    }
+    if(omg.toLowerCase() == correctText.toLowerCase()) {
+      console.log("You're correct!");
+      document.getElementById('nextBtn').innerHTML = "Next"
+    }
+    else {
+      console.log("That's incorrect");
+    }
+  });
+
+}
+
+function freerun() {
+
+  console.log("Yo!");
+
+  firebase.database().ref('freerun').once('value').then(function(snapshot) {
+    num = snapshot.val().num;
+    console.log("Num:" + num);
+
+    stuff(snapshot, num);
+
   });
 
 }

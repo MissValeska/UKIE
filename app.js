27bf108e9bb503e360db0663d2ab44e877f1293a
Valeska <missvaleskavv@gmail.com>
2017-08-20 03:20:06 -0700

    d

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require('express')
 const fs = require('fs');
 var path = require('path');
 const app = express();
+var io = require('socket.io')(80);
+
+var isLoggedin = false;
 
 app.set('port', (process.env.PORT || 3000));
 
 // set up static files
 app.use(express.static(path.join(__dirname, '/public/static')));
 
+io.on('connection', function(socket){
+  console.log("Hey");
+  socket.on('login', function(msg){
+    console.log("logged in, from app.js")
+    isLoggedin = true;
+  });
+  socket.on('logout', function(msg){
+    console.log("logged out, from app.js")
+    isLoggedin = false;
+  });
+});
 
 // set up views
 app.use(express.static(__dirname + '/views'));
@@ -15,23 +29,52 @@ app.set('view engine', 'pug');
 
 // show the home page (will also have our login links)
 app.get('/', function(req, res) {
-res.render('index')
+  if(isLoggedin) {
+    res.render('profile')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/index', function(req, res) {
-res.render('index')
+  if(isLoggedin) {
+    res.render('profile')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/index.html', function(req, res) {
-res.render('index')
+  if(isLoggedin) {
+    res.render('profile')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/inventory', function(req, res) {
-res.render('inventory')
+  if(isLoggedin) {
+    res.render('inventory')
+  }
+  else {
+    res.render('index')
+  }
+});
+
+app.get('/freerun', function(req, res) {
+  res.render('freerun')
 });
 
 app.get('/friends', function(req, res) {
-res.render('friends')
+  if(isLoggedin) {
+    res.render('friends')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/glossary', function(req, res) {
@@ -39,43 +82,86 @@ res.render('glossary')
 });
 
 app.get('/success', function (req, res) {
-res.render('success')
+  if(isLoggedin) {
+    res.render('success')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/profile', function (req, res) {
-res.render('profile')
+  if(isLoggedin) {
+    res.render('profile')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/results', function (req, res) {
-res.render('results')
+  if(isLoggedin) {
+    res.render('results')
+  }
+  else {
+    res.render('index')
+  }
 });
 
-app.get('/freerun1', function(req, res) {
-res.render('freerun1')
+app.get('/map', function(req, res) {
+  if(isLoggedin) {
+    res.render('map')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 
 app.get('/modules', function (req, res) {
-  res.render('modules')
+  if(isLoggedin) {
+    res.render('modules')
+  }
+  else {
+    res.render('index')
+  }
+});
+
+app.get('/freerun1', function (req, res) {
+  res.render('freerun1')
 });
 
 app.get('/module/:modNum', function (req, res) {
-  res.render('exercises')
+  if(isLoggedin) {
+    res.render('exercises')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/module/:modNum/exercise/:excNum', function (req, res) {
-res.render('questionblock')
+  if(isLoggedin) {
+    res.render('questionblock')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/module/:modNum/exercise/:excNum/questionblock/:qBlockNum/question/:qNum', function (req, res) {
-res.render('question')
+  if(isLoggedin) {
+    res.render('question')
+  }
+  else {
+    res.render('index')
+  }
 });
 
 app.get('/tos', function (req, res) {
   res.send("Nothing!");
 });
 
-
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port 3000')
+  console.log('Ukie app listening on port 3000')
 });
